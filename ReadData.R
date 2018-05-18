88c632eb01686c8dcc6a5791de596b7157b5c4de
innuganti <38195219+innuganti@users.noreply.github.com>
2018-05-17 20:30:21 -0400

    Add files via upload

---
+++ ReadData.R
@@ -0,0 +1,10 @@
+## Load the data 
+
+if(!file.exists("getdata-projectfiles-UCI HAR Dataset.zip")) {
+  temp <- tempfile()
+  download.file("http://d396qusza40orc.cloudfront.net/repdata%2Fdata%2Factivity.zip",temp)
+  unzip(temp)
+  unlink(temp)
+}
+
+data <- read.csv("activity.csv")
